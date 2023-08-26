3f790bb532f13f12816b91b72e70dfa9549b37ef
AlbertoLutz <albertolutzdias@gmail.com>
2023-08-26 11:29:52 -0300

    Manipulando múltiplas requisições em paralelo

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,20 +3,24 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonTypesToList(pokemonTypes) {
+  return pokemonTypes.map(
+    (typesSlot) => `<li class="type">${typesSlot.type.name}</li>`
+  );
+}
+
 function convertPokemonToLi(pokemon) {
   return `
     <li class="pokemon">
-          <span class="number">#001</span>
+          <span class="number">${pokemon.order}</span>
           <span class="name">${pokemon.name} </span>
 
           <div class="detail">
             <ol class="types">
-              <li class="type">grass</li>
-              <li class="type">poison</li>
-              <li></li>
+              ${convertPokemonTypesToList(pokemon.types).join("")}
             </ol>
             <img
-            src="https://raw.githubusercontent.com/jnovack/pokemon-svg/3c3ea26da58331d7202e7cdb1aab9b8347d8587f/svg/1.svg"
+            src="${pokemon.sprites.other.dream_world.front_default}"
             alt="${pokemon.name}"
           />
           </div>         
@@ -25,14 +29,6 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById("pokemonList");
 
-pokeApi.getPokemons().then((pokemons) => {
-  const listItems = [];
-
-  pokemon.map();
-
-  for (let i = 0; i < pokemons.length; i++) {
-    const pokemon = pokemons[i];
-    listItems.push(convertPokemonToLi(pokemon));
-  }
-  console.log(listItems);
+pokeApi.getPokemons().then((pokemons = []) => {
+  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
 });
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,16 @@
 const pokeApi = {};
 
+pokeApi.getPokemonsDetail = (pokemon) => {
+  return fetch(pokemon.url).then((response) => response.json());
+};
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
   return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.log(error));
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))
+    .then((detailsRequest) => Promise.all(detailsRequest))
+    .then((pokemonsDetails) => pokemonsDetails);
 };
