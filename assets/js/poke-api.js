3ba4b5a753ccadce2fdd9b14e1f551bc329c14ac
AlbertoLutz <albertolutzdias@gmail.com>
2023-08-27 20:14:38 -0300

    Limitando lista por geração, finalizando projeto

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,8 +1,8 @@
 const pokeApi = {};
 
-function convertPokeApíDetailToPokemon(pokeDetail) {
+function convertPokeApiDetailToPokemon(pokeDetail) {
   const pokemon = new Pokemon();
-  pokemon.number = pokeDetail.order;
+  pokemon.number = pokeDetail.id;
   pokemon.name = pokeDetail.name;
 
   const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
@@ -16,19 +16,19 @@ function convertPokeApíDetailToPokemon(pokeDetail) {
   return pokemon;
 }
 
-pokeApi.getPokemonsDetail = (pokemon) => {
+pokeApi.getPokemonDetail = (pokemon) => {
   return fetch(pokemon.url)
     .then((response) => response.json())
-    .then(convertPokeApíDetailToPokemon);
+    .then(convertPokeApiDetailToPokemon);
 };
 
-pokeApi.getPokemons = (offset = 0, limit = 50) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
   return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))
-    .then((detailsRequest) => Promise.all(detailsRequest))
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
     .then((pokemonsDetails) => pokemonsDetails);
 };
