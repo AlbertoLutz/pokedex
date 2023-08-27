22a9c217c3c9a0f0dbe87de08db2ad0aeedb3cbb
AlbertoLutz <albertolutzdias@gmail.com>
2023-08-27 18:19:28 -0300

    Convertendo modelo da PokeApi para o meu modelo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,17 @@
-const offset = 0;
-const limit = 10;
-
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
-function convertPokemonTypesToList(pokemonTypes) {
-  return pokemonTypes.map(
-    (typesSlot) => `<li class="type">${typesSlot.type.name}</li>`
-  );
-}
-
 function convertPokemonToLi(pokemon) {
   return `
-    <li class="pokemon">
-          <span class="number">${pokemon.order}</span>
+    <li class="pokemon" ${pokemon.type}>
+          <span class="number">${pokemon.number}</span>
           <span class="name">${pokemon.name} </span>
 
           <div class="detail">
             <ol class="types">
-              ${convertPokemonTypesToList(pokemon.types).join("")}
+              ${pokemon.types
+                .map((type) => `<li class="type">${type}</li>`)
+                .join("")}
             </ol>
             <img
-            src="${pokemon.sprites.other.dream_world.front_default}"
+            src="${pokemon.photo}"
             alt="${pokemon.name}"
           />
           </div>         
@@ -30,5 +21,6 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById("pokemonList");
 
 pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
+  const newHtml = pokemons.map(convertPokemonToLi).join("");
+  pokemonList.innerHTML = newHtml;
 });
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,7 +1,25 @@
 const pokeApi = {};
 
+function convertPokeApíDetailToPokemon(pokeDetail) {
+  const pokemon = new Pokemon();
+  pokemon.number = pokeDetail.order;
+  pokemon.name = pokeDetail.name;
+
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
+
+  pokemon.types = types;
+  pokemon.type = type;
+
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+  return pokemon;
+}
+
 pokeApi.getPokemonsDetail = (pokemon) => {
-  return fetch(pokemon.url).then((response) => response.json());
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApíDetailToPokemon);
 };
 
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
